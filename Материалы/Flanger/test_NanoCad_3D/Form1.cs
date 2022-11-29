189a294d99d990c93f3012b8ff0294638d0b56b9
NanoCAD-Mospolytech <117220296+NanoCAD-Mospolytech@users.noreply.github.com>
2022-11-29 18:32:27 +0300

    Add files via upload

---
+++ Материалы/Flanger/test_NanoCad_3D/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using test_NanoCad_3D;
+
+namespace test_NanoCad_3D
+{
+    public partial class Form1 : Form 
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public bool flag;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            flag = true;
+        }
+
+
+    }
+}
