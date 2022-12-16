227a97815af0c0f3b002bd12ef8d0ddbaefb80d9
NanoCAD-Mospolytech <117220296+NanoCAD-Mospolytech@users.noreply.github.com>
2022-12-16 11:04:11 +0300

    Add files via upload

---
+++ Материалы/Loft/MySample1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySample1
+{
+    static class Parametric
+    {
+        public static bool flagCmd1 = true;
+        public static int height;
+        public static int width;
+        public static int radius;
+    }
+}
